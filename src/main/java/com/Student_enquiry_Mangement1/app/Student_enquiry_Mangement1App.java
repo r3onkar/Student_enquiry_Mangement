9333f3044c6208cf4c25272e1b55424c65006a9e
r3onkar <onkarhulwan@gmail.com>
2024-01-04 21:46:56 +0530

    Initial Project Setup

---
+++ src/main/java/com/Student_enquiry_Mangement1/app/Student_enquiry_Mangement1App.java
@@ -0,0 +1,14 @@
+package com.Student_enquiry_Mangement1.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Student_enquiry_Mangement1App {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Student_enquiry_Mangement1App.class, args);
+		
+	}
+
+}
